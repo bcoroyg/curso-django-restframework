87b41f7ae9ac96f2d2af40bfb2e0313e3f7022b7
Emilio Coroy <becgonzalez25@gmail.com>
2023-10-04 09:54:45 -0600

    MODULO 5: Creando el token de autorizacion.

--- cride/users/serializers/users.py
+++ cride/users/serializers/users.py
@@ -5,6 +5,26 @@ from django.contrib.auth import authenticate
 
 # Django REST Framework
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+
+# Models
+from cride.users.models import User
+
+
+class UserModelSerializer(serializers.ModelSerializer):
+    """User model serializer."""
+
+    class Meta:
+        """Meta class."""
+
+        model = User
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'phone_number'
+        )
 
 
 class UserLoginSerializer(serializers.Serializer):
@@ -14,11 +34,17 @@ class UserLoginSerializer(serializers.Serializer):
     """
 
     email = serializers.EmailField()
-    password = serializers.CharField(min_length=8, max_length=64)
+    password = serializers.CharField(min_length=5, max_length=64)
 
     def validate(self, data):
         """Check credentials."""
         user = authenticate(username=data['email'], password=data['password'])
         if not user:
             raise serializers.ValidationError('Invalid credentials')
+        self.context['user'] = user
         return data
+
+    def create(self, data):
+        """Generate or retrieve new token."""
+        token, created = Token.objects.get_or_create(user=self.context['user'])
+        return self.context['user'], token.key
