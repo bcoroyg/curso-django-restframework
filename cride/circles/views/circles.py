f187abfbaf30a4183dcecbf7068dd4557526d7e2
Emilio Coroy <becgonzalez25@gmail.com>
2023-10-10 08:32:53 -0600

    MODULO 5: Update de circulo, custom permissions y DRF Mixins.

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -1,8 +1,11 @@
 """Circle views."""
 
 # Django REST Framework
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
+
+# Permissions
 from rest_framework.permissions import IsAuthenticated
+from cride.circles.permissions.circles import IsCircleAdmin
 
 # Serializers
 from cride.circles.serializers import CircleModelSerializer
@@ -11,7 +14,12 @@ from cride.circles.serializers import CircleModelSerializer
 from cride.circles.models import Circle, Membership
 
 
-class CircleViewSet(viewsets.ModelViewSet):
+class CircleViewSet(mixins.CreateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.ListModelMixin,
+                    # mixins.DestroyModelMixin,
+                    viewsets.GenericViewSet):
     """Circle view set."""
 
     queryset = Circle.objects.all()
@@ -24,6 +32,14 @@ class CircleViewSet(viewsets.ModelViewSet):
         if self.action == 'list':
             return queryset.filter(is_public=True)
         return queryset
+
+
+    def get_permissions(self):
+        """Assign permissions based on action."""
+        permissions = [IsAuthenticated]
+        if self.action in ['update', 'partial_update']:
+            permissions.append(IsCircleAdmin)
+        return [permission() for permission in permissions]
 
 
     def perform_create(self, serializer):
