74d6c6884512acc852a863bf19c72e24edfaf403
Emilio Coroy <becgonzalez25@gmail.com>
2023-10-12 14:50:11 -0600

    MODULO 5: Editar un ride.

--- cride/rides/views/rides.py
+++ cride/rides/views/rides.py
@@ -7,6 +7,7 @@ from rest_framework.generics import get_object_or_404
 # Permissions
 from rest_framework.permissions import IsAuthenticated
 from cride.circles.permissions.memberships import IsActiveCircleMember
+from cride.rides.permissions.rides import IsRideOwner
 
 # Filters
 from rest_framework.filters import SearchFilter, OrderingFilter
@@ -27,6 +28,7 @@ from django.utils import timezone
 
 class RideViewSet(mixins.ListModelMixin,
                   mixins.CreateModelMixin,
+                  mixins.UpdateModelMixin,
                   viewsets.GenericViewSet):
     """Ride view set."""
 
@@ -52,6 +54,14 @@ class RideViewSet(mixins.ListModelMixin,
         slug_name = kwargs['slug_name']
         self.circle = get_object_or_404(Circle, slug_name=slug_name)
         return super(RideViewSet, self).dispatch(request, *args, **kwargs)
+
+
+    def get_permissions(self):
+        """Assign permission based on action."""
+        permissions = [IsAuthenticated, IsActiveCircleMember]
+        if self.action in ['update', 'partial_update']:
+            permissions.append(IsRideOwner)
+        return [p() for p in permissions]
 
 
     def get_serializer_context(self):
